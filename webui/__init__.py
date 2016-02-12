b4e7187e797e940c6b87114962a8af379b1c5c95
Michel Beloshitsky <mbeloshitsky@nilksa.ru>
2016-02-12 17:13:02 +0500

    Webui Extensions

--- webui/__init__.py
+++ webui/__init__.py
@@ -17,10 +17,28 @@ def card2dict(card):
             output[rowcol[0]] = rowcol[1]
     return output
 
+def parse_raw(cards):
+    output = []
+    card   = None
+    lines = cards.split('\n')
+    for line in lines:
+        if line.strip().upper() == u'BEGIN:VCARD':
+            card = [line]
+        elif line.strip().upper() == u'END:VCARD':
+            card += [line]
+            output.append(u'\n'.join(card))
+            card = None
+        elif card != None:
+            card += [line]
+        else:
+            pass
+    return map(card2dict, output)
+
+
 @get('/')
 def index():
-    items = storage.ical.Collection.from_path('/test/carddav')
-    return haml_template(tmpl, entries=items[1:], parse=card2dict)
+    items = storage.ical.Collection.from_path('/nilksa/AddressBook')
+    return haml_template(tmpl, entries=parse_raw(items[1].text))
 
 @get('/static/<path:path>')
 def serve_static(path):
